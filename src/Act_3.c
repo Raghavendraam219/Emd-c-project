358ed651a2ac5837b6e1e35bb8eb1d60c0205148
Raghavendraam219 <73216258+Raghavendraam219@users.noreply.github.com>
2021-04-30 21:42:07 +0530

    Update Act_3.c

--- src/Act_3.c
+++ src/Act_3.c
@@ -12,51 +12,45 @@
 #include"Act_3.h"
 #include<util/delay.h>
 #include<avr/io.h>
-/**
- * @brief initializing timer and counter register
- * 
- */
+
+//initializing timer and counter register
 void Initialize_PWM(void)
 {
     /*Configuring the registers and ports*/
     TCCR1A|= (1<<COM1A1)|(1<<WGM10)|(1<<WGM11);
     TCCR1B|= (1<<WGM12)|(1<<CS11)|(1<<CS10);
     DDRB = DDRB | (1<<PB1);
-
 }
 
 char Output_PWM (uint16_t ADC_val)
 {
-    
-    char temperature;
-
+    char temp;
     if((ADC_val>=0) && (ADC_val<=209)){
-
         OCR1A = 205; 
-        temperature = 20;
+        temp = 20;
         _delay_ms(20);
     }
     else if((ADC_val>=210) && (ADC_val<=509)){
 
         OCR1A = 410; 
-        temperature = 25;
+        temp = 25;
        _delay_ms(20);
     }
     else if((ADC_val>=510) && (ADC_val<=709)){
 
         OCR1A = 717;
-        temperature = 29;
+        temp = 29;
         _delay_ms(20);
     }
     else if((ADC_val>=710) && (ADC_val<=1024)){
 
         OCR1A = 973; 
-        temperature = 33;
+        temp = 33;
         _delay_ms(20);
     }
     else{
         OCR1A = 0; 
-        temperature = 0;
+        temp = 0;
     }
-    return temperature;
+    return temp;
 }
