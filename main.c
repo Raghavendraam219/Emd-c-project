ae18065a1ef63f946f562e730e2095ec6fd446dc
Raghavendraam219 <73216258+Raghavendraam219@users.noreply.github.com>
2021-04-30 21:19:12 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,61 @@
-#include "project_config.h"
-
-#include "user_utils.h"
-#include "blinky.h"
-
 /**
- * @brief Initialize all the Peripherals and pin configurations
+ * @file Main.c
+ * @author Raghavendra A M
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
  * 
  */
+
+#include "Act_1.h"
+#include "Act_2.h"
+#include "Act_3.h"
+#include "Act_4.h"
+#include<avr/io.h>
+
 void peripheral_init(void)
 {
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
+    /*Configure LED and Switch pins*/
+    LED();
+    /*Configure ADC registers and pins*/
+    Initilize_ADC();
+    /*Configure PWM registers and pins*/
+    Initialize_PWM();
+    /*Configure UART serial communication pin*/
+    Initialize_UART(103);
 }
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
-/**
- * @brief Main function where the code execution starts
- * 
- * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
- */
+    
+   
+uint16_t temp;
+char temp_data;
 int main(void)
 {
-	/* Initialize Peripherals */
-	peripheral_init();
-
-	for(;;)
-	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
-	}
-	return 0;
+    /*uint16_t temp;*/
+    // Initialize peripherals
+    peripheral_init();
+    while(1)
+    {
+        if(SENSOR_ON) //If switch_1 is ON
+        {
+            if(HEAT_ON) //If switch_2 is ON
+            {
+                state1(LED_ON);//LED is ON
+                temp=Read_ADC(0);
+                temp_data = Output_PWM(temp);
+                UART_write(temp_data);
+            }
+            else
+            {  
+                state1(LED_OFF);
+            }
+        }
+        else
+        {
+            state1(LED_OFF);//LED is OFF
+            OCR1A=0;
+        }
+    }
+    return 0;
 }
