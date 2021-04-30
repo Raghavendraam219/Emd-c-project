2d7dd61f4bfdae2056ed259edde24126c6e0ceaa
Raghavendraam219 <73216258+Raghavendraam219@users.noreply.github.com>
2021-04-30 23:53:54 +0530

    Update Act_4.c

--- src/Act_4.c
+++ src/Act_4.c
@@ -10,16 +10,16 @@
  */
 #include<avr/io.h>
 #include"Act_4.h"
-
-void Initialize_UART(uint16_t ubrr_value){
-
+//Initialising the values for UART
+void Initialize_UART(uint16_t ubrr_value)
+{
     UBRR0L = ubrr_value;
     UBRR0H = (ubrr_value>>8) & (0x00ff);
     UCSR0C = (1<<UMSEL00) | (1<<UCSZ01) | (1<<UCSZ01);
     UCSR0B = (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0) | (1<<TXCIE0);
 }
 
-void UART_write(char data)
+void UART_write(char data)        //Writing to UART 
 {
     while(!(UCSR0A & (1<<UDRE0)))
     {
